ced4a65f73fcad6b1ba77d92d194da843fa206da
mosama88 <mosama88@hotmail>
2025-01-23 19:25:09 +0200

    Done Go2

--- resources/views/components/import-excel.blade.php
+++ resources/views/components/import-excel.blade.php
@@ -3,11 +3,12 @@
 </button>
 
 
-<div class="modal fade" id="import_excel_file">
-    <div class="modal-dialog">
+<div class="modal fade" id="import_excel_file" tabindex="-1" role="dialog" aria-labelledby="importExcelModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">{{ __('action.upload_excel_file') }}</h4>
+                <h5 class="modal-title" id="importExcelModalLabel">Import Excel File</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -16,25 +17,19 @@
                 @csrf
                 <input type="hidden" name="model" value="{{ $model }}">
                 <div class="modal-body">
-                    <p>{{ __('action.choose_file_excel') }}&hellip;</p>
-
-                    <div class="mb-3">
-                        <label for="formFile" name="file" class="form-label"></label>
-                        <input class="form-control" type="file" id="formFile">
+                    <div class="form-group">
+                        <label for="file">Select Excel File</label>
+                        <input type="file" name="file" id="file" class="form-control" required>
                     </div>
-
                 </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Save changes</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Upload</button>
                 </div>
             </form>
         </div>
-        <!-- /.modal-content -->
     </div>
-    <!-- /.modal-dialog -->
 </div>
-
 
 {{-- @push('js')
     <script>
