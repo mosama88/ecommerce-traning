aafb26010ad389b1548e38e12020457e1c0a5044
mosama <mosama88@mail.com>
2025-01-26 13:04:45 +0200

    Done Go3

--- resources/views/dashboard/partials/actions.blade.php
+++ resources/views/dashboard/partials/actions.blade.php
@@ -11,13 +11,44 @@
                 class="fas fa-eye mx-1"></i>
             {{ __('action.show') }}</a>
         <div class="dropdown-divider"></div>
-
-        <form action="{{ route('dashboard.' . $name . '.destroy', $name_id) }}" method="POST">
+        <form id="delete-form-{{ $name_id }}" action="{{ route('dashboard.' . $name . '.destroy', $name_id) }}"
+            method="POST" style="display: none;">
             @csrf
             @method('DELETE')
-            <button type="submit" class="dropdown-item text-danger">
-                <i class="fas fa-trash-alt mx-1"></i>
-                {{ __('action.delete') }}</button>
         </form>
+        <button id="delete_one" data-id="{{ $name_id }}" class="dropdown-item text-danger delete-btn">
+            <i class="fas fa-trash-alt mx-1"></i>
+            {{ __('action.delete') }}
+        </button>
+
     </div>
 </div>
+
+@push('js')
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // إضافة مستمع للنقر على أزرار الحذف
+            document.querySelectorAll('.delete-btn').forEach(function(button) {
+                button.addEventListener('click', function(event) {
+                    event.preventDefault(); // منع تقديم النموذج بشكل افتراضي
+                    const nameId = this.getAttribute('data-id');
+                    const form = document.getElementById(`delete-form-${nameId}`);
+
+                    Swal.fire({
+                        title: "{{ __('action.are_you_sure') }}",
+                        text: "{{ __('action.you_are_about_to_delete_selected_items') }}",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonText: "{{ __('action.delete_them') }}",
+                        cancelButtonText: "{{ __('action.no_cancel') }}",
+                        reverseButtons: true
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            form.submit(); // تقديم النموذج بعد تأكيد الحذف
+                        }
+                    });
+                });
+            });
+        });
+    </script>
+@endpush
