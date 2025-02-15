4b4e41d1c30f24e944a0dde4fdbdbe1324a15641
mosama <mosama88@mail.com>
2025-02-15 12:43:36 +0200

    Discount3

--- app/Http/Controllers/Dashboard/ReportController.php
+++ app/Http/Controllers/Dashboard/ReportController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ReportController extends Controller
 {
-    
+    function books()
+    {
+        return view('dashboard.reports.sales.books');
+    }
+    function revenue()
+    {
+        return view('dashboard.reports.sales.revenue');
+    }
+    function trends()
+    {
+        return view('dashboard.reports.sales.trends');
+    }
 }
---
+++ resources/views/dashboard/reports/sales/revenue.blade.php
@@ -0,0 +1,36 @@
+@extends('dashboard.layouts.layouts')
+@section('title', 'Books Report')
+@section('content_header')
+    <div class="row d-flex flex-row mb-3">
+        <div class="col-sm-6">
+            <ol class="breadcrumb">
+                <x-breadcrumb :breadcrumbs="generate_breadcrumbs()" />
+            </ol>
+        </div>
+       
+    </div>
+   
+
+@stop
+
+@section('content')
+
+    {{-- {{ dd(session('success')) }} --}}
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <!-- /.card-header -->
+                <div class="card-body p-0">
+                   
+                    {{-- Here content --}}
+                    <h1>Hello Revenue</h1>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+    </div>
+
+
+@stop
